46d9b1930b81c5b7b533eab9a3fd43e9f2f60a00
Prasant Adhikari <pa1038@nyu.edu>
2025-12-05 14:18:14 +0400

    refactored day1 solutions

--- src/solution/day1.rs
+++ src/solution/day1.rs
@@ -5,23 +5,20 @@ use crate::Solution;
 pub struct Part1;
 pub struct Part2;
 
+pub struct Action {
+    direction: i64,
+    magnitude: i64,
+}
+
 impl Solution for Part1 {
     fn solve(&self, filename: &str) -> i64 {
-        let actions = fs::read_to_string(filename)
-            .expect("Should have been able to read day1.txt as input file");
-        let actions = actions.split("\n");
-        let mut answer: i64 = 0;
-        let mut position: i64 = 50;
+        let actions = read_input(filename);
+        let mut position = 50;
+        let mut answer = 0;
         for action in actions {
-            position += match action.chars().nth(0) {
-                Some('L') => -1 * action[1..].parse::<i64>().unwrap(),
-                Some('R') => action[1..].parse::<i64>().unwrap(),
-                _ => 0,
-            };
+            position += action.direction * action.magnitude;
             position %= 100;
-            if position == 0 {
-                answer += 1;
-            }
+            answer += (position == 0) as i64
         }
         return answer;
     }
@@ -29,35 +26,31 @@ impl Solution for Part1 {
 
 impl Solution for Part2 {
     fn solve(&self, filename: &str) -> i64 {
-        let actions = fs::read_to_string(filename)
-            .expect(format!("Should have been able to read {filename} as input file").as_str());
-        let actions = actions.split("\n");
-        let mut answer: i64 = 0;
-        let mut position: i64 = 50;
-        for action in actions {
-            let delta = match action.chars().nth(0) {
-                Some('L') => -1 * action[1..].parse::<i64>().unwrap(),
-                Some('R') => action[1..].parse::<i64>().unwrap(),
-                _ => 0,
-            };
-            match delta {
-                mut v if delta < 0 => {
-                    while v != 0 {
-                        position = (position - 1) % 100;
-                        v += 1;
-                        answer += if position == 0 { 1 } else { 0 };
-                    }
-                }
-                mut v if v > 0 => {
-                    while v != 0 {
-                        position = (position + 1) % 100;
-                        v -= 1;
-                        answer += if position == 0 { 1 } else { 0 };
-                    }
-                }
-                _ => (),
+        let actions = read_input(filename);
+        let mut position = 50;
+        let mut answer = 0;
+        for mut action in actions {
+            while action.magnitude != 0 {
+                position = (position + action.direction) % 100;
+                action.magnitude -= 1;
+                answer += (position == 0) as i64
             }
         }
         return answer;
     }
+}
+
+fn read_input(filename: &str) -> Vec<Action> {
+    let actions = fs::read_to_string(filename)
+        .expect(format!("Should have been able to read {} as input file", filename).as_str());
+    let actions = actions.trim().split("\n");
+    actions
+        .filter_map(|action| match action.trim().chars().nth(0) {
+            Some(v) => Some(Action {
+                direction: if v == 'L' { -1 } else { 1 },
+                magnitude: action[1..].parse().ok()?,
+            }),
+            _ => None,
+        })
+        .collect()
 }
